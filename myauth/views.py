78af515e8d73264fdc73ef547769e00027ea3b3c
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-01-26 13:44:35 +0100

    Minor modification of my auth views + error message handled differently

--- myauth/views.py
+++ myauth/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 from . import forms
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 
 class LogoutView(View, LoginRequiredMixin):
@@ -13,10 +14,6 @@ class LogoutView(View, LoginRequiredMixin):
         logout(request)
         return redirect(self.redirect_field_name)
 
-# def logout_user(request):
-#     logout(request)
-#     return redirect(settings.LOGOUT_REDIRECT_URL)
-
 
 class LoginView(View):
     template_name = 'myauth/login.html'
@@ -24,8 +21,7 @@ class LoginView(View):
 
     def get(self, request):
         form = self.form_class()
-        message = ''
-        return render(request, self.template_name, context={'form': form, 'message': message})
+        return render(request, self.template_name, context={'form': form})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -37,27 +33,9 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 return redirect('home')
-        message = 'Identifiants invalides.'
-        return render(request, self.template_name, context={'form': form, 'message': message})
-
-
-# def login_page(request):
-#     form = forms.LoginForm()
-#     message = ''
-#     if request.method == 'POST':
-#         form = forms.LoginForm(request.POST)
-#         if form.is_valid():
-#             user = authenticate(
-#                 username=form.cleaned_data['username'],
-#                 password=form.cleaned_data['password']
-#             )
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('home')
-#             else:
-#                 message = 'Identifiants invalides.'
-#     return render(
-#         request, 'myauth/login.html', context={'form': form, 'message': message})
+            else:
+                messages.error(request, "Nom d'utilisateur ou mot de passe incorrect.")
+        return render(request, self.template_name, context={'form': form})
 
 
 class SignupView(View):
@@ -75,14 +53,3 @@ class SignupView(View):
             login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
         return render(request, self.template_name, context={'form': form})
-
-
-# def signup_page(request):
-#     form = forms.SignupForm()
-#     if request.method == 'POST':
-#         form = forms.SignupForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect(settings.LOGIN_REDIRECT_URL)
-#     return render(request, 'myauth/signup.html', context={'form': form})
