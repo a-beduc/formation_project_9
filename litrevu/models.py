b021e146932a00a920c1b163d011e88f73624239
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-01-26 13:46:45 +0100

    Addition of a model in litrevu + UserBlocks

--- litrevu/models.py
+++ litrevu/models.py
@@ -36,3 +36,12 @@ class UserFollows(models.Model):
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
         unique_together = ('user', 'followed_user',)
+
+
+class UserBlocks(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blocking')
+    blocked_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                     related_name='blocked_by')
+
+    class Meta:
+        unique_together = ('user', 'blocked_user',)
