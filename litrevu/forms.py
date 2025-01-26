d83c2755d4d573b5fc270950519dbc0edcea8cc9
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-01-26 13:46:15 +0100

    Addition of two simple forms in litrevu + UserFollowsForm + UserBlocksForm

--- litrevu/forms.py
+++ litrevu/forms.py
@@ -12,3 +12,24 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.Review
         fields = ['headline', 'rating', 'body']
+
+        def __init__(self):
+            pass
+
+
+class UserFollowsForm(forms.Form):
+    username = forms.CharField(
+        max_length=128,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': "Nom d'utilisateur",
+                                      'class': 'newRelationBlock__input'}),
+    )
+
+
+class UserBlocksForm(forms.Form):
+    username = forms.CharField(
+        max_length=128,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': "Nom d'utilisateur",
+                                      'class': 'newRelationBlock__input'}),
+    )
