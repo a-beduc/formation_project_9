2d91a54e2d935e04a2d4df2e11aa2539e156cfa4
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-01-26 13:42:21 +0100

    Refactoring myauth forms

--- myauth/forms.py
+++ myauth/forms.py
@@ -4,11 +4,46 @@ from django.contrib.auth import get_user_model
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label="Nom d'utilisateur")
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label="Mot de passe")
+    username = forms.CharField(max_length=63,
+                               label="",
+                               widget=forms.TextInput(
+                                   attrs={"placeholder": "Nom d'utilisateur",
+                                          "class": 'loginBlock__input'}
+                                )
+                               )
+    password = forms.CharField(max_length=63,
+                               label="",
+                               widget=forms.PasswordInput(
+                                   attrs={"placeholder": "Mot de passe",
+                                          "class": 'loginBlock__input'}
+                                )
+                               )
 
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username',)
+        fields = ('username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+
+        self.fields['username'].label = ''
+        self.fields['password1'].label = ''
+        self.fields['password2'].label = ''
+
+        self.fields['username'].widget.attrs.update({
+            'placeholder': "Nom d'utilisateur",
+            'class': 'signupPage__input',
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': "Mot de passe",
+            'class': 'signupPage__input',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': "Confirmer mot de passe",
+            'class': 'signupPage__input',
+        })
