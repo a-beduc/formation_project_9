61339fe273010a6cdacf33fc8261561c0c5119e9
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-02-25 06:35:14 +0100

    Minor modification + Addition of the UserBlocks relations to the admin panel

--- litrevu/admin.py
+++ litrevu/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from litrevu.models import Ticket, Review, UserFollows
+from litrevu.models import Ticket, Review, UserFollows, UserBlocks
+
+
+
+
 
 admin.site.register(Ticket)
 admin.site.register(Review)
 admin.site.register(UserFollows)
+admin.site.register(UserBlocks)
