05f3a502bf58595d2dec90b9b4c7a4537553d41b
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-02-25 17:20:32 +0100

    Refactoring Scripts + Deleting useless code + Adding comments

--- mystatic/js/customBrowse.js
+++ mystatic/js/customBrowse.js
@@ -1,4 +1,6 @@
-
+/**
+ * Script to manage image upload through a custom button.
+ */
 document.addEventListener("DOMContentLoaded", () => {
     const fileInput = document.querySelector(".formTicket__hidden");
     const customButton = document.querySelector(".formTicket__customBlock__upload");
@@ -6,13 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const checkbox = document.querySelector('.formTicket__checkbox');
     const deleteButton = document.querySelector(".formTicket__customBlock__delete");
 
+    /**
+     * When user clicks the "custom" button. Trigger a click on the hidden file input.
+     */
     customButton.addEventListener("click", () => {
         fileInput.click();
-        if (checkbox) {
-            checkbox.checked = false;
-        }
     })
 
+    /**
+     * Display the name of the file selected before upload.
+     */
     fileInput.addEventListener("change", () => {
         if (fileName) {
             fileName.style.display = "inline-block";
@@ -24,6 +29,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    /**
+     * Check the hidden checkbox when clicking on the custom delete button.
+     */
     if (deleteButton) {
         deleteButton.addEventListener("click", () => {
             if (checkbox) {
@@ -35,4 +43,3 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     }
 })
-
--- mystatic/js/customRating.js
+++ mystatic/js/customRating.js
@@ -1,8 +1,17 @@
+/**
+ * Script to highlight star elements based on a user-selected rating
+ * and synchronize that selection with a hidden form input.
+ */
 document.addEventListener("DOMContentLoaded", () => {
     const ratingInput = document.querySelector(".formReview__hidden");
     const stars = document.getElementsByClassName("formReview__stars__elem");
     const starContainer = document.querySelector(".formReview__stars");
 
+    /**
+     * Highlights the stars from index up to index n (included)
+     * Updates the hidden rating input to the selected value of n.
+     * @param n - integer
+     */
     window.gfg = function(n) {
         removeStars();
         for (let i = 0; i < n+1; i++) {
@@ -11,6 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
         ratingInput.value = n;
     }
 
+    /**
+     * Removes the highlight of all the stars.
+     */
     function removeStars() {
         let i = 1;
         while (i < 6) {
@@ -20,6 +32,8 @@ document.addEventListener("DOMContentLoaded", () => {
         ratingInput.value = 0;
     }
 
+    // Read the initial rating of a ticket if it exists and color the corresponding stars,
+    // used when modifying a Review.
     let reviewRating = parseInt(starContainer.getAttribute("data-rating"), 10);
     gfg(reviewRating);
 })
